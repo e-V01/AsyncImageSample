c4361dc6aea2827d338bef0090a987cdbd0aced6
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-12-13 16:17:48 +0200

    animation added

--- AsyncImageSample/ContentView.swift
+++ AsyncImageSample/ContentView.swift
@@ -33,15 +33,48 @@ struct ContentView: View {
 //        AsyncImage(url: URL(string: imageUrl), scale: 3.0)
         
         // MARK: 3. Placeholer
-        AsyncImage(url: URL(string: imageUrl)) { image in
-            image.imageModifier()
-                
-        } placeholder: {
-            Image(systemName: "photo.circle.fill")
-                .iconModifier()
+//        AsyncImage(url: URL(string: imageUrl)) { image in
+//            image.imageModifier()
+//                
+//        } placeholder: {
+//            Image(systemName: "photo.circle.fill")
+//                .iconModifier()
+//        }
+//        .padding(40)
+        // MARK: 4. Phase
+//        AsyncImage(url: URL(string: imageUrl)) { phase in
+//            // success
+//            //failure
+//            // empty
+//            if let image = phase.image {
+//                image.imageModifier()
+//            } else if  phase.error != nil {
+//                Image(systemName: "ant.circle.fill").iconModifier()
+//            } else {
+//                Image(systemName: "photo.circle.fill").iconModifier()
+//            }
+//        }
+        //        .padding(40)
+        // MARK: 5. Animation
+        AsyncImage(url: URL(string: imageUrl),
+                   transaction: Transaction(animation: .spring(response: 0.5,
+                                                               dampingFraction: 0.6,
+                                                               blendDuration: 0.25))) { phase in
+            switch phase {
+            case .success(let image):
+                image.imageModifier()
+//                    .transition(.move(edge: .top))
+//                    .transition(.slide)
+                    .transition(.scale)
+            case . failure(_):
+                Image(systemName: "ant.circle.fill").iconModifier()
+            case .empty:
+                Image(systemName: "photo.circle.fill").iconModifier()
+            @unknown default:
+                ProgressView()
+            }
         }
-        .padding(40)
-            
+                                                               .padding(40)
     }
 }
 
