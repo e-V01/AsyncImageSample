f7e3666edbd7bc73a6e1f66241ec83fd80b1af01
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-12-13 15:02:00 +0200

    Initial Commit

---
+++ AsyncImageSample/AsyncImageSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageSampleApp.swift
+//  AsyncImageSample
+//
+//  Created by Y K on 13.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
